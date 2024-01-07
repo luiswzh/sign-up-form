a95f0e2557696304973d0af1c0b31c19046195eb
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2024-01-07 18:55:48 -0500

    Add password validation

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("test")
+const pass = document.querySelector('#password');
+const passCheck = document.querySelector('#confirm-password')
+const form = document.querySelector('form');
+const checkError = document.querySelector('.error');
+
+form.addEventListener('submit', (event)=>{
+    if(!(pass.value==passCheck.value)){
+        console.log('hey its not the same password')
+        event.preventDefault();
+        checkError.textContent='*passwords don\'t match';
+    };
+});
